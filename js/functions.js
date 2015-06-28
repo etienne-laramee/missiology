9e68bf89b2cba08bd3bc9e78e09202f920334f82
cacapicante <antigravity_liquid@hotmail.com>
2015-06-28 01:35:56 -0400

    fixed footer. modified sidebar for subbar

--- js/functions.js
+++ js/functions.js
@@ -2,20 +2,49 @@ $("document").ready(function($) {
 	var nav = $('.nav-container');
 	var bannerHeight = 300;
 	$(window).scroll(function() {
+		var scrollTop = $(this).scrollTop();
+		
+		// Nav fix when scroll
 		var navHeight = bannerHeight;
-		if($(this).scrollTop() > navHeight) {
+		if(scrollTop > navHeight) {
 			nav.addClass("f-nav-container");
 			//nav.removeClass('nav-container');
 		} else {
 			nav.removeClass("f-nav-container");
 			//nav.addClass('nav-container');
 		}
+		
+		fixFooter();
 	});
 	
 	// Select default tabs and content
 	main_nav('home');
 	side_nav('the-objectives');
 });
+
+function fixFooter()
+{
+		var scrollTop = $(window).scrollTop();
+		
+		// Footer fix when scroll
+		var windowHeight = $(window).height();
+		var mainFooter = $('.main-footer');
+		var mainContent = $('.main-content');
+	
+		var footerPosition = mainFooter.position();
+		var contentPosition = mainContent.position();
+		var contentBottom = (contentPosition.top + mainContent.height());
+		var bottomWindow = (windowHeight + scrollTop);
+		if(((contentBottom + 150) < bottomWindow) && (footerPosition.top <= bottomWindow))
+		{
+			mainFooter.addClass('f-footer');
+		}
+		else
+		{
+			mainFooter.removeClass('f-footer');
+		}
+
+}
 
 // Main menu tabs
 function main_nav(tabName)
@@ -35,6 +64,9 @@ function side_nav(tabName)
 	highlightSideTab(tabName);
 	
 	setContent(tabName);
+	
+	// Unfix footer
+	fixFooter();
 }
 // /Side Bar tabs
 
@@ -78,7 +110,7 @@ function setSidebar(tab)
 	var relatedTabs = document.getElementsByClassName(tab);
 	for(i = 0; i < relatedTabs.length; i++)
 	{
-		relatedTabs[i].style.display = "block";
+		relatedTabs[i].style.display = "inline-block";
 	}
 }
 
